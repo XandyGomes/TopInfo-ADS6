f8b9c4ebac23c16270c76cf1e86be8e00ff7916e
Alexandre.Gomes <alexandre.silva251@fatec.sp.gov.br>
2022-11-14 22:14:52 +0000

    Aula.Final.12.11.2022

--- lib/screens/contatos/listaContatos.dart
+++ lib/screens/contatos/listaContatos.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:topinfoads6/database/app_database.dart';
+import 'package:topinfoads6/models/contato.dart';
 import 'formularioContatos.dart';
 
 const _tituloAppbar = 'Contatos';
 
-class ListaContatos extends StatelessWidget {
+class ListaContatos extends StatefulWidget {
+  @override
+  State<ListaContatos> createState() => _ListaContatosState();
+}
+
+class _ListaContatosState extends State<ListaContatos> {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -17,21 +24,39 @@ class ListaContatos extends StatelessWidget {
           ],
         ),
       ),
-      body: ListView(
-        children: [
-          Card(
-            child: ListTile(
-              title: Text(
-                'Alexandre',
-                style: TextStyle(fontSize: 24.0),
-              ),
-              subtitle: Text(
-                '4689-8',
-                style: TextStyle(fontSize: 16.0),
-              ),
-            ),
-          ),
-        ],
+      body: FutureBuilder(
+        initialData: [],
+        future: findAll(),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+              // TODO: Handle this case.
+              break;
+            case ConnectionState.waiting:
+              return Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    CircularProgressIndicator(),
+                    Text('Loading'),
+                  ],
+                ),
+              );
+            case ConnectionState.active:
+              // TODO: Handle this case.
+              break;
+            case ConnectionState.done:
+              final List<Contato> contatos = snapshot.data as List<Contato>;
+              return ListView.builder(
+                itemBuilder: (context, index) {
+                  final Contato contato = contatos[index];
+                  return _ItemContato(contato);
+                },
+                itemCount: contatos.length,
+              );
+          }
+          return Text('Unkown Error');
+        },
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
@@ -42,12 +67,33 @@ class ListaContatos extends StatelessWidget {
                 ),
               )
               .then(
-                (newContato) => debugPrint(
-                  newContato.toString(),
-                ),
+                (value) => setState(() {}),
               );
         },
         child: Icon(Icons.person_add_alt),
+      ),
+    );
+  }
+}
+
+class _ItemContato extends StatelessWidget {
+  final Contato contato;
+
+  _ItemContato(this.contato);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Card(
+      child: ListTile(
+        title: Text(
+          contato.name,
+          style: TextStyle(fontSize: 24.0),
+        ),
+        subtitle: Text(
+          contato.accountNumber.toString(),
+          style: TextStyle(fontSize: 16.0),
+        ),
       ),
     );
   }
